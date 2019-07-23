5146119996e5a0324254429c28d9f0b7e701f720
kwrsin@gmail.com <kwrsin@gmail.com>
2019-07-23 15:11:21 +0900

    camera clamping

--- components/camera.lua
+++ components/camera.lua
@@ -2,6 +2,8 @@ return function(parent, width, height, child, target, world_width, world_height)
   local M = {}
   local root = display.newGroup()
   root:insert(child)
+  root.x = -width / 2
+  root.y = -height / 2
 
   function M:set_focus(target)
     M.target = target
@@ -14,16 +16,18 @@ return function(parent, width, height, child, target, world_width, world_height)
       target_width = M.target.width / 2
       target_height = M.target.height / 2
     end
-    -- M.child.x = math.max(math.min(M.child.x, world_width - width / 2), width / 2)
-    M.child.y = math.max(math.min(M.child.y, world_height - height / 2), height / 2)
+    -- M.child.x = math.max(math.min(M.child.x, world_width - width), width / 2)
+    -- M.child.y = math.max(math.min(M.child.y, world_height - height), height / 2)
+    M.child.x = math.min(math.max(M.child.x, 0), world_width - width)
+    M.child.y = math.min(math.max(M.child.y, 0), world_height - height)
   end
 
   local function enterFrame()
     if M.target then
       M.child.x = - (M.target.x - width / 4)
       M.child.y = - (M.target.y - height / 4)
-
       clamp()
+
     end
 
   end
@@ -36,10 +40,18 @@ return function(parent, width, height, child, target, world_width, world_height)
     Runtime:removeEventListener("enterFrame", enterFrame)
   end
 
-  local container = display.newContainer(parent, width, height, target)
+  local container = display.newContainer(width, height)
+  -- container.anchorChildren = false
+  container.anchorX = 0
+  container.anchorY = 0
+  container.width = world_width
+  container.height = world_height
+  -- root:insert(container)
   M.container = container
   M.child = child
   M.container:insert(root)
+  -- M.container:translate(display.contentCenterX, display.contentCenterY)
+  parent:insert(container)
   M:set_focus(target)
 
 
