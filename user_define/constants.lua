3a5ec224358b75c413cf72acadad6d814db252ee
kwrsin <kwrsin@gmail.com>
2019-07-29 20:32:01 +0900

    arrange constants

--- user_define/constants.lua
+++ user_define/constants.lua
@@ -3,20 +3,21 @@ M.CONTINUE = -1
 M.BREAK = 0
 M.NEXT = 1
 
--- TILEMAP
-M.MAP_PATH = 'assets.fghi'
-
--- FRAME
-M.FRAME_PATH = 'assets.abcde'
-
-
 -- DEFAULT AUDIO
 M.BBS_AUDIO_PATH = "assets/audio/on.wav"
 M.MODAL_AUDIO_PATH = "assets/audio/selected.wav"
 
--- EXTRA AUDIO
-M.BBS_AUDIO_PATH_ETC = "assets/audio/tap.wav"
-M.OK_AUDIO_PATH = "assets/audio/ok.wav"
-M.NG_AUDIO_PATH = "assets/audio/ng.wav"
+-- SCENE 1
+  -- TILEMAP
+  M.MAP_PATH = 'assets.fghi'
+
+-- SCENE 2
+  -- FRAME
+  M.FRAME_PATH = 'assets.abcde'
+
+  -- EXTRA AUDIO
+  M.BBS_AUDIO_PATH_ETC = "assets/audio/tap.wav"
+  M.OK_AUDIO_PATH = "assets/audio/ok.wav"
+  M.NG_AUDIO_PATH = "assets/audio/ng.wav"
 
 return M
